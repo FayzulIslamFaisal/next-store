4788f94273ab8db67416dc849c4d8574f18ffe83
Fayzul Islam Faisal <mfbfaisal@gmail.com>
2024-05-09 13:46:30 +0600

    home slider issue fixed

--- app/services/getHomeSlider.js
+++ app/services/getHomeSlider.js
@@ -1,9 +1,9 @@
 export const getHomeSlider = async () => {
     try {
-        const response = await fetch( `https://v3.nagadhat.com/api/get-sliders` );
+        const response = await fetch(`https://v3.nagadhat.com/api/get-sliders`, { next: { revalidate: 10 } });
         return await response.json();
     } catch (error) {
-        console.error( 'Something went wrong fetching slider data' );
+        console.error('Something went wrong fetching slider data');
         console.info(error);
     }
 }
