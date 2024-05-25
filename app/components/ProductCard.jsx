55e73b177e43594ddb4e4b83ae598d6f6f8be572
Fayzul Islam Faisal <mfbfaisal@gmail.com>
2024-05-25 18:16:56 +0600

    flas sale api connect

--- app/components/ProductCard.jsx
+++ app/components/ProductCard.jsx
@@ -1,30 +1,33 @@
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import AddToCartButton from "./AddToCartButton";
+import Like from "./Like";
 
 function ProductCard({ item }) {
-    return (
-        <div className="flash-sale-content-item">
-            <div className="flash-sale-content-bg nh-hover-box-shadow">
-                <div className="flash-sale-content-img image-hover-effect">
-                    <img
-                        src={item.image}
-                        className="img-fluid"
-                        alt={item.altText}
-                    />
-                </div>
-                <div className="flash-sale-content-info text-hover-effect">
-                    <h4>
-                        <Link href={item.path}>{item.title}</Link>
-                    </h4>
-                    <strong>৳ {item.price}</strong>
-                    <div className="add-to-cart-holder d-flex align-items-center justify-content-between ">
-                        {item.addToCart}
-                        {item.like}
-                    </div>
-                </div>
-            </div>
+  const image = `https://v3.nagadhat.com/${item.product_thumbnail}`;
+  const { product_name: title, slug, mrp_price: price } = item;
+  return (
+    <div className="flash-sale-content-item">
+      <div className="flash-sale-content-bg nh-hover-box-shadow d-flex flex-column justify-content-between">
+        <div className="flash-sale-content-img image-hover-effect">
+          <Image src={image} alt={title} fill={true} />
         </div>
-    );
+        <div className="flash-sale-content-info text-hover-effect">
+          <div className="">
+            <h4>
+              <Link href={slug ?? ""}>{title}</Link>
+            </h4>
+            <strong>৳ {price}</strong>
+          </div>
+          <div className="add-to-cart-holder d-flex align-items-center justify-content-between ">
+            <AddToCartButton />
+            <Like />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ProductCard;
