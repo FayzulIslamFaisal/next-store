6281e50b018c72c666f9f6c0d120e696b1cd42c4
Azhar uddin rahad <azheruddinrahad@gmail.com>
2024-05-28 11:16:14 +0600

    azhar initial commit

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,14 +1,11 @@
 function NotFound() {
-    return (
-        <div className="container d-flex justify-content-center align-items-center">
-            <div
-                className="alert alert-warning col-12 mt-3 text-center"
-                role="alert"
-            >
-                Nothing found
-            </div>
-        </div>
-    );
+  return (
+    <div className="container d-flex justify-content-center align-items-center">
+      <div className="alert alert-warning col-12 mt-3 text-center" role="alert">
+        Nothing Found
+      </div>
+    </div>
+  );
 }
 
 export default NotFound;
