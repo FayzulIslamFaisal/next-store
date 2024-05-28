97a71a6658743f012bc0d98c495a6542d2c6ba32
Azhar uddin rahad <azheruddinrahad@gmail.com>
2024-05-28 18:19:26 +0600

    This commmit belongs to previous commit

--- app/[slug]/page.jsx
+++ app/[slug]/page.jsx
@@ -1,9 +1,33 @@
+import Head from "next/head";
 import Sales from "../components/Sales";
 import Service from "../components/Service";
 import Breadcrumb from "../components/productDetail/Breadcrumb";
 import ProductLeftSide from "../components/productDetail/ProductLeftSide";
 import ProductRightSide from "../components/productDetail/ProductRightSide";
 
+export const metadata = {
+  openGraph: {
+    title: "Next.js",
+    description: "The React Framework for the Web",
+    url: "https://nextjs.org",
+    siteName: "Next.js",
+    images: [
+      {
+        url: "https://static-01.daraz.com.bd/p/57703e043589d286263bb30487d44fd4.jpg", // Must be an absolute URL
+        width: 800,
+        height: 600,
+      },
+      {
+        url: "https://static-01.daraz.com.bd/p/57703e043589d286263bb30487d44fd4.jpg", // Must be an absolute URL
+        width: 1800,
+        height: 1600,
+        alt: "My custom alt",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+};
 const ProductSinglePage = ({ params }) => {
   const { slug } = params;
 
@@ -37,6 +61,7 @@ const ProductSinglePage = ({ params }) => {
   return (
     <>
       <section className="product-details-section">
+        <Head>{metadata}</Head>
         <div className="container">
           <Breadcrumb />
           <div className="row product-details-info">
--- app/components/productDetail/ProductLeftSide.jsx
+++ app/components/productDetail/ProductLeftSide.jsx
@@ -6,28 +6,6 @@ import ProductLongDescription from "./ProductLongDescription";
 const ProductLeftSide = () => {
   return (
     <>
-      <Head>
-        <title>Welcome to the Product Page</title>
-        <meta property="og:title" content="Welcome to the Product Page" />
-        <meta property="og:description" content="Check out this product" />
-        <meta
-          property="og:image"
-          content="https://nagadhat.com.bd/storage/media/project_images/1674733711_Discount-Partners-Web-Slider.jpg"
-        />
-        <meta
-          property="og:url"
-          content="https://next-store-self-five.vercel.app/"
-        />
-        <meta property="og:type" content="website" />
-        <meta property="og:site_name" content="Nagadhat" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Welcome to the Product Page" />
-        <meta name="twitter:description" content="Check out this product" />
-        <meta
-          name="twitter:image"
-          content="https://nagadhat.com.bd/storage/media/project_images/1674733711_Discount-Partners-Web-Slider.jpg"
-        />
-      </Head>
       <div className=" col-xl-9">
         <div className="row">
           <ProductImage />
