eb18faafc3de0f4e0312cc7878a27920d7e945a9
rajankarmakar <rajankarmakar@hotmail.com>
2024-06-11 17:05:10 +0600

    Fix deployment erron in main

--- app/layout.js
+++ app/layout.js
@@ -5,6 +5,7 @@ import Footer from "./components/Footer";
 import "./globals.css";
 import DistrictModal from "./components/DistrictModal";
 import AuthProvider from "./auth/Provider";
+import { Suspense } from 'react';
 
 const lato = Lato({
   subsets: ['latin'],
@@ -61,12 +62,14 @@ export default function RootLayout({ children }) {
       <Script strategy="afterInteractive" src="/js/bootstrap.bundle.min.js" />
 
       <body>
-        <AuthProvider>
-          <Header />
-          <DistrictModal />
-          {children}
-          <Footer />
-        </AuthProvider>
+        <Suspense>
+          <AuthProvider>
+            <Header />
+            <DistrictModal />
+            {children}
+            <Footer />
+          </AuthProvider>
+        </Suspense>
       </body>
     </html>
   );
