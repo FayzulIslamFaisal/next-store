e6b4079c60e6e1ec2249be2431bdee2a0d06c21f
rajankarmakar <rajankarmakar@hotmail.com>
2024-06-22 11:45:34 +0600

    Fix category page issue

--- app/components/productCategory/CategoryProductArchiveItems.jsx
+++ app/components/productCategory/CategoryProductArchiveItems.jsx
@@ -2,42 +2,46 @@ import Image from "next/image";
 import Link from "next/link";
 // import StarRating from "./StarRating";
 import AddToCartButton from "../AddToCartButton";
+import { NagadhatPublicUrl } from "../../utils";
 
 const CategoryProductArchiveItems = ({ productItem }) => {
-  let imageUrl = null;
-  const { product_name: title, slug: path, mrp_price: price } = productItem;
-  const slicedTitle = title?.length > 43 ? `${title.slice(0, 43)} ...` : title;
-  if (productItem.product_thumbnail) {
-    imageUrl = `https://v3.nagadhat.com/${productItem.product_thumbnail}`;
-  }
-  return (
-    <div className="flash-sale-content-item">
-      <Link href={path ? path : "#"}>
-        <div className="flash-sale-content-bg nh-hover-box-shadow">
-          <div className="product-category-image">
-            <div className="flash-sale-content-img image-hover-effect">
-              <Image
-                fill={true}
-                src={imageUrl}
-                className="img-fluid"
-                alt={title}
-              />
-            </div>
-          </div>
-          <div className="flash-sale-content-info text-hover-effect">
-            <h4>{slicedTitle}</h4>
-            <div className="category-product-price d-flex align-items-center justify-content-between  ">
-              <strong>৳ {price}</strong>
-              {/* <StarRating ratingCount={ratingCount} ratingImg={ratingImg} /> */}
-            </div>
-            <div className="add-to-cart-holder">
-              <AddToCartButton fullWidth={`category-product-add-btn`} />
-            </div>
-          </div>
+    let imageUrl = null;
+    const { product_name: title, slug: path, mrp_price: price } = productItem;
+    const slicedTitle =
+        title?.length > 43 ? `${title.slice(0, 43)} ...` : title;
+    if (productItem.product_thumbnail) {
+        imageUrl = `${NagadhatPublicUrl}/${productItem.product_thumbnail}`;
+    }
+    return (
+        <div className="flash-sale-content-item">
+            <Link href={path ? path : "#"}>
+                <div className="flash-sale-content-bg nh-hover-box-shadow">
+                    <div className="product-category-image">
+                        <div className="flash-sale-content-img image-hover-effect">
+                            <Image
+                                fill={true}
+                                src={imageUrl}
+                                className="img-fluid"
+                                alt={title}
+                            />
+                        </div>
+                    </div>
+                    <div className="flash-sale-content-info text-hover-effect">
+                        <h4>{slicedTitle}</h4>
+                        <div className="category-product-price d-flex align-items-center justify-content-between  ">
+                            <strong>৳ {price}</strong>
+                            {/* <StarRating ratingCount={ratingCount} ratingImg={ratingImg} /> */}
+                        </div>
+                        <div className="add-to-cart-holder">
+                            <AddToCartButton
+                                fullWidth={`category-product-add-btn`}
+                            />
+                        </div>
+                    </div>
+                </div>
+            </Link>
         </div>
-      </Link>
-    </div>
-  );
+    );
 };
 
 export default CategoryProductArchiveItems;
--- app/components/productCategory/SubCategoryList.jsx
+++ app/components/productCategory/SubCategoryList.jsx
@@ -1,20 +1,24 @@
 import SubCategoryListItems from "./SubCategoryListItems";
 
 const SubCategoryList = ({ subCategoryData }) => {
-    const hasLongSubTitle = subCategoryData.some((item) => item?.title.length > 10);
-  return (
-    <div className={`product-sub-category-area sub-category-pb40 ${hasLongSubTitle ? 'sub-category-fixed-height' : ''}`}>
-      <ul >
-        {Array.isArray(subCategoryData) &&
-          subCategoryData.map((categoryItem) => (
-            <SubCategoryListItems
-              key={categoryItem.id}
-              categoryItem={categoryItem}
-            />
-          ))}
-      </ul>
-    </div>
-  );
+    return (
+        <div
+            className={`product-sub-category-area sub-category-pb40 ${
+                subCategoryData.length >= 10 ? "sub-category-fixed-height" : ""
+            }`}
+        >
+            <ul>
+                {Array.isArray(subCategoryData) &&
+                    subCategoryData.length &&
+                    subCategoryData.map((categoryItem) => (
+                        <SubCategoryListItems
+                            key={categoryItem.id}
+                            categoryItem={categoryItem}
+                        />
+                    ))}
+            </ul>
+        </div>
+    );
 };
 
 export default SubCategoryList;
