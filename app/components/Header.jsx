1f9a3fa17ef585dbd63650d9d704b05859656eb9
anzamHossen <akashlog402@gmail.com>
2024-07-14 17:23:35 +0600

    Same user in credentials and google

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -59,7 +59,7 @@ function Header() {
 
     console.log('=>>> get login status from header page', status);
     console.log('=>>> get login session from header page', session);
-    // console.log('=>>> get login token from header page', session?.accessToken);
+    console.log('=>>> get login token from header page', session?.accessToken);
 
     useEffect(() => {
         storeUserAgent();
