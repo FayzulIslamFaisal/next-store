70b2150038de68ec0381ea8e72b41949eae783c2
MD Naem <mmnayeem408@gmail.com>
2024-08-12 17:09:58 +0600

    category and brand pagination

--- app/services/getHomeCategory.js
+++ app/services/getHomeCategory.js
@@ -1,10 +1,12 @@
 import {filterByStatus, apiBaseUrl} from '../utils';
 
-export const getHomeCategory = async () => {
+export const getHomeCategory = async (page = 1, limit = 24) => {
     try {
-        const response = await fetch(`${apiBaseUrl}/all-category`, { next: { revalidate: 10 } });
+        const response = await fetch(`${apiBaseUrl}/all-category?page=${page}&limit=${limit}`, { next: { revalidate: 10 } });
         const data = await response.json();
-        return filterByStatus(data.results.category);
+        console.log(data);
+        
+        return data.results.category;
     } catch (error) {
         console.error('Something went wrong fetching Home Category data');
         console.info(error);
--- app/viewall/page.jsx
+++ app/viewall/page.jsx
@@ -1,14 +1,45 @@
-// import Pagination from "../components/productCategory/Pagination";
+
 import Service from "../components/Service";
 import ViewAllBanner from "../components/viewAllProduct/ViewAllBanner";
 import ViewAllCategoryTitle from "../components/viewAllProduct/ViewAllCategoryTitle";
 import ViewAllCategories from "../components/viewAllProduct/ViewAllCategories";
+import ViewAllFlashCheck from "../components/viewAllProduct/ViewAllFlashCheck";
 import { getHomeCategory } from "../services/getHomeCategory";
 import { getHomeBrand } from "../services/getHomeBrand";
 import { NagadhatPublicUrl } from "../utils";
-import ViewAllFlashCheck from "../components/viewAllProduct/ViewAllFlashCheck";
+import CategoryPagination from "../components/productCategory/CategoryPagination";
 
 const ViewAllPage = async ({ searchParams }) => {
+    const page = parseInt(searchParams.page) || 1;
+    let viewCategoryData = [];
+    let sectionTitle = "View all";
+    let bannerUrl = "/images/fashion.jpg";
+    let lastPage = 1;
+    const limit = 24; //Per Page Category
+
+    switch (searchParams.type) {
+        case "category":
+            const categoryData = await getHomeCategory(page, limit);
+            viewCategoryData = categoryData.data;
+            lastPage = categoryData.last_page;
+            if (viewCategoryData?.length >= 1 && viewCategoryData[0].banner_image) {
+                bannerUrl = `${NagadhatPublicUrl}/${viewCategoryData[0].banner_image}`;
+            }
+            sectionTitle = "Categories";
+            break;
+        case "brand":
+            const brandData = await getHomeBrand(page, limit);
+            viewCategoryData = brandData.results.brands.data;
+            lastPage = brandData.results.brands.last_page;
+            if (viewCategoryData?.length >= 1 && viewCategoryData[0].banner) {
+                bannerUrl = `${NagadhatPublicUrl}/${viewCategoryData[0].banner}`;
+            }
+            sectionTitle = "Brands";
+            break;
+        default:
+            break;
+    }
+
     const serviceItems = [
         {
             imageurl: "/images/pickup.svg",
@@ -36,48 +67,20 @@ const ViewAllPage = async ({ searchParams }) => {
         },
     ];
 
-    let viewCategoryData = [];
-    let sectionTitle = "View all";
-    let bannerUrl = "/images/fashion.jpg";
-    if (searchParams) {
-        switch (searchParams.type) {
-            case "category":
-                viewCategoryData = await getHomeCategory();
-
-                if (
-                    viewCategoryData.length >= 1 &&
-                    viewCategoryData[1].banner_image
-                ) {
-                    bannerUrl = `${NagadhatPublicUrl}/${viewCategoryData[1].banner_image}`;
-                }
-                sectionTitle = "Categories";
-                break;
-            case "brand":
-                const data = await getHomeBrand();
-                viewCategoryData = data.results.brands;
-                if (
-                    viewCategoryData.length >= 1 &&
-                    viewCategoryData[1].banner
-                ) {
-                    bannerUrl = `${NagadhatPublicUrl}/${viewCategoryData[1].banner}`;
-                }
-                sectionTitle = "Brands";
-                break;
-            default:
-                break;
-        }
-    }
 
     return (
         <div className="container view-all-product-container">
             <ViewAllBanner imageUrl={bannerUrl} />
             <ViewAllCategoryTitle title={sectionTitle} />
             <ViewAllCategories viewCategoryData={viewCategoryData} />
-            {/* <div className="row view-all-product-pagination-area">
-                <div className="col-md-12 text-center">
-                    <Pagination />
+            <div className="view-all-product-pagination-area ">
+                <div className="d-flex justify-content-center align-items-center">
+                    <CategoryPagination
+                        currentPage={page} 
+                        lastPage={lastPage} 
+                    />
                 </div>
-            </div> */}
+            </div>
             <ViewAllFlashCheck />
             <Service serviceItems={serviceItems} />
         </div>
