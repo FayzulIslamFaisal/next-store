c04cba1517498fee9741b8a41d902d2f300a2c9f
MD Naem <121675549+naem0@users.noreply.github.com>
2024-08-28 18:44:52 +0600

    affiliated container booking page functional

--- app/components/customerDashboard/affiliate/affiliateproducts/ContainerOrderDetails.jsx
+++ app/components/customerDashboard/affiliate/affiliateproducts/ContainerOrderDetails.jsx
@@ -1,90 +1,159 @@
-const ContainerOrderDetails = () => {
+"use client"
+import { NagadhatPublicUrl } from "@/app/utils";
+import { useEffect, useState } from "react";
+import { FaMinus, FaPlus, FaTrashAlt } from "react-icons/fa";
+
+const ContainerOrderDetails = ({ selectedProducts, setSelectedProducts }) => {
+    // Initialize state with selectedProducts and default quantity
+    
+    console.log({ selectedProducts });
+
+    const handleIncrease = (productId) => {
+        setSelectedProducts(prevProducts =>
+            prevProducts.map(product =>
+                product.id === productId ? { ...product, quantity: product.quantity + 1 } : product
+            )
+        );
+    };
+
+    const handleDecrease = (productId) => {
+        setSelectedProducts(prevProducts =>
+            prevProducts.map(product =>
+                product.id === productId && product.quantity > 1 ? { ...product, quantity: product.quantity - 1 } : product
+            )
+        );
+    };
+
+    const handleQuantityChange = (productId, event) => {
+        const newQuantity = parseInt(event.target.value, 10);
+
+        if (!isNaN(newQuantity) && newQuantity >= 1 && newQuantity <= 500) {
+            setSelectedProducts(prevProducts =>
+                prevProducts.map(product =>
+                    product.id === productId ? { ...product, quantity: newQuantity } : product
+                )
+            );
+        }
+    };
+
+    const handleDeleteSelectedProducts = (productId) => {
+        const updatedProducts = selectedProducts.filter(product => product.id !== productId);
+        setSelectedProducts(updatedProducts); // Update the local state
+    };
+
+
+    const calculateTotalPrice = () => {
+        return selectedProducts.reduce((acc, product) => acc + product.pivot.mrp_price * product.quantity, 0);
+    };
+    const calculateDiscount = () => {
+        return selectedProducts.reduce((acc, product) => acc + product.pivot.profit * product.quantity, 0);
+    };
+
+    const totalPrice = calculateTotalPrice();
+    const discount = calculateDiscount();
+    const finalTotal = totalPrice - discount;
+
     return (
         <>
             <div className="row gy-3 p-4">
-                <div className="col-md-9 overflow-x-auto">
+                <div className="col-md-8 overflow-x-auto">
                     <div
-                        className="container-booking-oder-table "
+                        className="container-booking-oder-table"
                         style={{ minWidth: "400px" }}
                     >
                         <h3>Order Details</h3>
-                        <table className="table table-borderless">
+                        <table className="table">
                             <thead>
                                 <tr>
                                     <th>Product</th>
                                     <th>Details</th>
                                     <th>Quantity</th>
                                     <th>Price</th>
+                                    <th></th>
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr>
-                                    <td>
-                                        <img
-                                            style={{
-                                                height: "60px",
-                                            }}
-                                            src="/images/flash-img3.jpg"
-                                            alt="hello"
-                                        />
-                                    </td>
-                                    <td>Maggi Masala 620gm (8Pcs)</td>
-                                    <td>
-                                        <div
-                                            className="btn-group quantity-area container-booking-quantity-area"
-                                            role="group"
-                                            aria-label="Basic example"
-                                        >
-                                            <button
-                                                type="button"
-                                                className="quantity-increase"
-                                            >
-                                                <i className="fa-solid fa-minus"></i>
-                                            </button>
-                                            <input
-                                                className="quantity-fild"
-                                                min="1"
-                                                max="5"
-                                                step="1"
-                                                autoComplete="off"
-                                                height="100%"
-                                                readOnly=""
-                                                type="text"
-                                                defaultValue="1"
+                                {selectedProducts.map((product) => (
+                                    <tr key={product.id}>
+                                        <td>
+                                            <img
+                                                style={{ height: "50px" }}
+                                                src={`${NagadhatPublicUrl}/${product.product_thumbnail}`}
+                                                alt={product.product_name}
                                             />
-                                            <button
-                                                className="quantity-decrease"
-                                                type="button"
+                                        </td>
+                                        <td>
+                                            {product.product_name}
+                                        </td>
+                                        <td className="align-middle">
+                                            <div
+                                                className="btn-group px-1 quantity-area container-booking-quantity-area"
+                                                role="group"
+                                                aria-label="Basic example"
                                             >
-                                                <i className="fa-solid fa-plus"></i>
-                                            </button>
-                                        </div>
-                                    </td>
-                                    <td>
-                                        <strong>1000</strong>
-                                    </td>
-                                </tr>
+                                                <button
+                                                    type="button"
+                                                    className="quantity-decrease"
+                                                    onClick={() => handleDecrease(product.id)}
+                                                    style={{ fontSize: "16px" }}
+                                                >
+                                                    <FaMinus />
+                                                </button>
+                                                <input
+                                                    className="quantity-fild py-1"
+                                                    min="1"
+                                                    max="500"
+                                                    type="text"
+                                                    style={{ width: "36px" }}
+                                                    value={product.quantity}
+                                                    onChange={(e) => handleQuantityChange(product.id, e)}
+                                                />
+                                                <button
+                                                    className="quantity-increase"
+                                                    type="button"
+                                                    onClick={() => handleIncrease(product.id)}
+                                                    style={{ fontSize: "16px" }}
+                                                >
+                                                    <FaPlus />
+                                                </button>
+                                            </div>
+                                        </td>
+                                        <td className="align-middle">
+                                            <strong>{product.pivot.mrp_price * product.quantity}</strong>
+                                        </td>
+                                        <td className="align-middle">
+                                            <p
+                                                onClick={()=>handleDeleteSelectedProducts(product.id)}
+                                                className="text-danger"
+                                                title="Delete"
+                                                style={{cursor:"pointer"}}
+                                            >
+                                                <FaTrashAlt />
+                                            </p>
+                                        </td>
+                                    </tr>
+                                ))}
                             </tbody>
                         </table>
                     </div>
                 </div>
-                <div className="col-md-3">
+                <div className="col-md-4">
                     <div className="container-booking-oder-table">
-                        <h3>Oder Amount</h3>
+                        <h3>Order Amount</h3>
                         <table className="table table-borderless container-booking-oder-amount-table">
                             <tbody>
                                 <tr>
                                     <td>Sub Total</td>
-                                    <td>4000</td>
+                                    <td>{totalPrice}</td>
                                 </tr>
                                 <tr>
                                     <td>Discount</td>
-                                    <td>200</td>
+                                    <td>{discount}</td>
                                 </tr>
                                 <tr>
                                     <td>Total</td>
                                     <td>
-                                        <strong>3800</strong>
+                                        <strong>{finalTotal}</strong>
                                     </td>
                                 </tr>
                             </tbody>
