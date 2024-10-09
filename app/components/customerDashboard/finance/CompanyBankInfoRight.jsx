107232562f3cb4a46f48127ca7501a753e1a7fe8
Fayzul Islam Faisal <mfbfaisal@gmail.com>
2024-10-09 16:06:20 +0600

    order view page added in invoice

--- app/components/customerDashboard/finance/CompanyBankInfoRight.jsx
+++ app/components/customerDashboard/finance/CompanyBankInfoRight.jsx
@@ -2,10 +2,10 @@ import Image from "next/image";
 const CompanyBankInfoRight = () => {
     return (
         <>
-            <div className="col-md-6">
+            <div className="col-md-5">
                 <div
                     className=" position-relative w-100 "
-                    style={{ height: "250px" }}
+                    style={{ height: "320px" }}
                 >
                     <Image fill src={`/images/bkash-image.png`} alt="image" />
                 </div>
