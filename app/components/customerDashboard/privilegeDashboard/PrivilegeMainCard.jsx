d6d5e02542b1b65ec60a426ad2f431ee7ed1bc1a
Fayzul Islam Faisal <mfbfaisal@gmail.com>
2024-10-23 18:32:19 +0600

    getPrivilegeCardDetails API Implement

--- app/components/customerDashboard/privilegeDashboard/PrivilegeMainCard.jsx
+++ app/components/customerDashboard/privilegeDashboard/PrivilegeMainCard.jsx
@@ -1,53 +1,57 @@
+import { NagadhatPublicUrl } from "@/app/utils";
 import Image from "next/image";
 
-const PrivilegeMainCard = () => {
+const PrivilegeMainCard = ({ privilegeCardInfo }) => {
+    const backImageUrl = privilegeCardInfo?.privilege_card?.back_image || "/images/priv-membership-back.png";
+    const frontImageUrl = privilegeCardInfo?.privilege_card?.front_image || "/images/1665226202_1.png";
+
     return (
-        <>
-            <div className="customer-dashboard-order-history-title">
-                <div className=" flipper-container-wrapp">
-                    <div className="flipper-container">
-                        <div className="flip">
-                            <div className="front face ">
-                                <Image
-                                    src={`/images/1665226202_1.png`}
-                                    alt="image"
-                                    style={{ objectFit: "cover" }}
-                                    fill
-                                />
-                            </div>
-                            <div className="back face">
-                                <Image
-                                    src={`/images/priv-membership-back.png`}
-                                    alt="image"
-                                    style={{ objectFit: "cover" }}
-                                    fill
-                                />
-                            </div>
+        <div className="customer-dashboard-order-history-title">
+            <div className="flipper-container-wrapp">
+                <div className="flipper-container">
+                    <div className="flip">
+                        <div className="front face">
+                            <Image
+                                src={`${NagadhatPublicUrl}/${frontImageUrl}`}
+                                alt="Privilege card front"
+                                layout="fill"
+                                objectFit="cover"
+                            />
                         </div>
-                    </div>
-                    <div className="px-3 py-3 ">
-                        <strong className="fs-5 pb-3">৳ 1000</strong>
-                        <h6 className="fs-6">Membership Card</h6>
-                        <div className="pt-2 d-flex align-items-center gap-2 ">
-                            <button
-                                type="button"
-                                className="add-to-cart-link border-0 rounded-3 text-capitalize "
-                                data-bs-toggle="modal"
-                                data-bs-target="#exampleModal"
-                            >
-                                Details
-                            </button>
-                            <button className="add-to-cart-link border-0 rounded-3 text-capitalize ">
-                                Active
-                            </button>
-                            <button className="add-to-cart-link border-0 rounded-3 text-capitalize ">
-                                Buy
-                            </button>
+                        <div className="back face">
+                            <Image
+                                src={`${NagadhatPublicUrl}/${backImageUrl}`}
+                                alt="Privilege card back"
+                                layout="fill"
+                                objectFit="cover"
+                            />
                         </div>
                     </div>
                 </div>
+                <div className="px-3 py-3">
+                    <strong className="fs-5 pb-3">
+                        ৳ {privilegeCardInfo?.purchases_price}
+                    </strong>
+                    <h6 className="fs-6">{privilegeCardInfo?.product_name}</h6>
+                    <div className="pt-2 d-flex align-items-center gap-2">
+                        <button
+                            type="button"
+                            className="add-to-cart-link border-0 rounded-3 text-capitalize"
+                            data-bs-toggle="modal"
+                            data-bs-target="#exampleModal"
+                        >
+                            Details
+                        </button>
+                        <button className="add-to-cart-link border-0 rounded-3 text-capitalize">
+                            Active
+                        </button>
+                        <button className="add-to-cart-link border-0 rounded-3 text-capitalize">
+                            Buy
+                        </button>
+                    </div>
+                </div>
             </div>
-        </>
+        </div>
     );
 };
 
